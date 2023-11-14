4d9fea4e16b667d678d68b72d6d0425dc93209ac
Markov Andrey <andremarkov@icloud.com>
2023-11-14 22:49:03 +0300

    feat: start project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="bg-gradient-to-r from-blue-950 to-sky-800 w-full h-screen p-24">
+      <div className="text-center">
+        <h1>Mythic Library</h1>
+        <h2>Персонажи</h2>
+        <p>Привет</p>
+      </div>
+    </main>
+  )
+}
