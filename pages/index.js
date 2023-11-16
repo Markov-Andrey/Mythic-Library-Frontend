465b9d5a5241fb0debdebdd78f31302ed9edf9ca
Markov Andrey <andremarkov@icloud.com>
2023-11-16 22:35:33 +0300

    feat: initial collection of character information

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,22 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Image from 'next/image';
+import Head from 'next/head';
+const storage = process.env.NEXT_PUBLIC_BACKEND_STORAGE;
 
-export default function Home() {
-    const storage = process.env.NEXT_PUBLIC_BACKEND_STORAGE;
-    const [character, setCharacter] = useState("");
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('http://mythic-library.com/api/character/1');
-                setCharacter(response.data);
-            } catch (error) {
-                console.error('Error fetching character:', error);
-            }
-        };
-        fetchData();
-    }, []);
-
+const RecursiveArrayRenderer = ({ data }) => {
     return (
         <div>
-            <h1>Character Details</h1>
-            <div className="text-3xl font-bold underline">Hello</div>
-            {character && (
-                <ul>
-                    {Object.entries(character).map(([key, value]) => (
-                        <li key={key}>
-                            {key === 'logo' ? (
+            {Object.entries(data).map(([key, value]) => (
+                <div key={key}>
+                    {typeof value === 'object' && value !== null ? (
+                        <>
+                            {key !== '0' && <h2>{key}</h2>}
+                            <RecursiveArrayRenderer data={value} />
+                        </>
+                    ) : (
+                        <>
+                            {['icon', 'image', 'logo'].includes(key) ? (
                                 <>
                                     <strong>{key}:</strong>
                                     <div className="rounded-full overflow-hidden">
@@ -44,10 +34,39 @@ export default function Home() {
                                     <strong>{key}:</strong> {value}
                                 </>
                             )}
-                        </li>
-                    ))}
-                </ul>
-            )}
+                        </>
+                    )}
+                </div>
+            ))}
         </div>
     );
-}
+};
+
+const Home = () => {
+    const [character, setCharacter] = useState("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://mythic-library.com/api/character/1');
+                setCharacter(response.data);
+                console.log(response.data);
+            } catch (error) {
+                console.error('Error fetching character:', error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <Head>
+                <title>Персонаж: { character.info.name }</title>
+            </Head>
+            <h1>{ character.info.name }</h1>
+            {character && <RecursiveArrayRenderer data={character} />}
+        </div>
+    );
+};
+
+export default Home;
