0865579aa68accade1d493820de979550d423bba
Andre Markov <andremarkov@icloud.com>
2024-11-06 11:51:24 +0300

    add abilities page

---
+++ pages/[session]/abilities.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="p-5">
+        <h1 class="text-3xl font-bold mb-4">Предметы</h1>
+        <div v-if="loading" class="text-lg">Загрузка...</div>
+        <div v-else-if="error" class="text-red-500">{{ error }}</div>
+        <div v-else>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                <div v-for="item in locations" :key="item.id" class="border rounded-lg shadow p-4">
+                    <img v-if="item.image" :src="getImageUrl(item.image)" alt="Item Image" class="w-full h-40 object-cover rounded mb-2" />
+                    <h2 class="text-lg font-semibold">{{ item.name }}</h2>
+                    <p class="text-gray-600">{{ item.description }}</p>
+                    <p><strong>Тип:</strong> {{ item.type }}</p>
+                    <div v-if="item.parameters && Object.keys(item.parameters).length" class="mt-2">
+                        <h3 class="text-sm font-semibold">Свойства:</h3>
+                        <ul class="list-disc text-sm pl-5">
+                            <li v-for="(value, key) in item.parameters" :key="key">{{ key }}: {{ value }}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+import { useRoute, useRouter } from 'vue-router';
+import { useRuntimeConfig } from '#app';
+
+const locations = ref([]);
+const loading = ref(true);
+const error = ref(null);
+const route = useRoute();
+const router = useRouter();
+const config = useRuntimeConfig();
+
+const fetchItems = async (id) => {
+    error.value = null;
+    try {
+        const { data } = await axios.post(`${config.public.apiBase}/api/abilities/${id}`, {
+            type: null,
+        }, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('auth_token')}`
+            }
+        });
+        locations.value = data;
+    } catch (error) {
+        error.value = `Ошибка при загрузке данных: ${error.response ? error.response.data.message : error.message}`;
+    } finally {
+        loading.value = false;
+    }
+};
+
+const getImageUrl = (imageName) => `${config.public.apiBase}/storage/abilities/${imageName}`;
+
+onMounted(() => {
+    const sessionId = route.params.session;
+    fetchItems(sessionId);
+});
+</script>
