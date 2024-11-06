f8669e6310d3888d3fc4439c67c2e5f6a1877ab6
Andre Markov <andremarkov@icloud.com>
2024-11-06 12:07:15 +0300

    add characters page

--- pages/[session]/characters.vue
+++ pages/[session]/characters.vue
@@ -1,19 +1,20 @@
 <template>
     <div class="p-5">
-        <h1 class="text-3xl font-bold mb-4">Способности</h1>
+        <h1 class="text-3xl font-bold mb-4">Персонажи</h1>
         <div v-if="loading" class="text-lg">Загрузка...</div>
         <div v-else-if="error" class="text-red-500">{{ error }}</div>
         <div v-else>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 <div v-for="item in locations" :key="item.id" class="border rounded-lg shadow p-4">
-                    <img v-if="item.image" :src="getImageUrl(item.image)" alt="Item Image" class="w-full h-40 object-cover rounded mb-2" />
+                    <img v-if="item.avatar" :src="getImageUrl(item.avatar)" alt="Item Image" class="w-full h-40 object-cover rounded mb-2" />
                     <h2 class="text-lg font-semibold">{{ item.name }}</h2>
-                    <p class="text-gray-600">{{ item.description }}</p>
-                    <p><strong>Тип:</strong> {{ item.type }}</p>
-                    <div v-if="item.parameters && Object.keys(item.parameters).length" class="mt-2">
-                        <h3 class="text-sm font-semibold">Свойства:</h3>
+                    <p class="text-gray-600">{{ item.info }}</p>
+                    <p><strong>Здоровье:</strong> {{ item.health }}/{{ item.max_health }}</p>
+                    <p><strong>Опыт:</strong> {{ item.experience }}</p>
+                    <div v-if="item.attributes && Object.keys(item.attributes).length" class="mt-2">
+                        <h3 class="text-sm font-semibold">Параметры:</h3>
                         <ul class="list-disc text-sm pl-5">
-                            <li v-for="(value, key) in item.parameters" :key="key">{{ key }}: {{ value }}</li>
+                            <li v-for="(value, key) in item.attributes" :key="key">{{ key }}: {{ value }}</li>
                         </ul>
                     </div>
                 </div>
@@ -38,7 +39,7 @@ const config = useRuntimeConfig();
 const fetchItems = async (id) => {
     error.value = null;
     try {
-        const { data } = await axios.post(`${config.public.apiBase}/api/abilities/${id}`, {
+        const { data } = await axios.post(`${config.public.apiBase}/api/characters/${id}`, {
             type: null,
         }, {
             headers: {
@@ -53,7 +54,7 @@ const fetchItems = async (id) => {
     }
 };
 
-const getImageUrl = (imageName) => `${config.public.apiBase}/storage/abilities/${imageName}`;
+const getImageUrl = (imageName) => `${config.public.apiBase}/storage/avatars/${imageName}`;
 
 onMounted(() => {
     const sessionId = route.params.session;
