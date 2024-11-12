21448933089426bc9ff4989013e965b237d2c28f
Andre Markov <andremarkov@icloud.com>
2024-11-12 15:36:49 +0300

    add item page

--- pages/session/[session]/item/[id].vue
+++ pages/session/[session]/item/[id].vue
@@ -4,20 +4,20 @@
     <div v-else>
         <div class="p-5 grid gap-4">
             <div>
-                <img :src="item.image_url" alt="Item Image" class="w-[300px] h-[300px] object-cover rounded mb-2" />
+                <img :src="item.image_url" alt="Item Image" class="h-[300px] object-cover rounded mb-2" />
                 <h2 class="text-lg font-semibold">{{ item.name }}</h2>
                 <p class="text-gray-600">{{ item.description }}</p>
                 <p><strong>Тип:</strong> {{ item.type }}</p>
-                <p><strong>Вес:</strong> {{ item.weight_per_unit }} кг</p>
-                <p><strong>Цена:</strong> {{ item.value }} ₽</p>
-                <div v-if="item.properties && Object.keys(item.properties).length" class="mt-2">
+                <p><strong>Вес:</strong> {{ item.weight_per_unit }}</p>
+                <p><strong>Цена:</strong> {{ item.value }}</p>
+                <div v-if="item.has_hidden_properties">
+                    <p class="text-sm italic text-gray-500">Скрытые свойства</p>
+                </div>
+                <div v-else-if="item.properties && Object.keys(item.properties).length" class="mt-2">
                     <h3 class="text-sm font-semibold">Свойства:</h3>
                     <ul class="list-disc text-sm pl-5">
                         <li v-for="(value, key) in item.properties" :key="key">{{ key }}: {{ value }}</li>
                     </ul>
-                </div>
-                <div v-else-if="item.has_hidden_properties">
-                    <p class="text-sm italic text-gray-500">Есть скрытые свойства</p>
                 </div>
                 <div v-else>
                     <p class="text-sm text-gray-500">Нет дополнительных свойств</p>
@@ -35,7 +35,7 @@ import { useRuntimeConfig } from '#app';
 import { useHead } from "@vueuse/head";
 
 useHead({
-    title: 'Детали предмета',
+    title: 'Предметы',
 });
 
 const item = ref({});
@@ -60,6 +60,14 @@ const fetchItem = async (id) => {
         loading.value = false;
     }
 };
+
+watchEffect(() => {
+    if (item.value?.name) {
+        useHead({
+            title: item.value.name,
+        });
+    }
+});
 
 onMounted(() => {
     const itemId = route.params.id;
